31db04bdaf69c3ec406d27bfa790e7d91d919cc3
raynertjx <rayner742@gmail.com>
2025-06-28 16:39:08 +0800

    feat: add user queries

---
+++ internal/db/queries/users.sql
@@ -0,0 +1,13 @@
+-- name: CreateUser :one
+INSERT INTO users (username, password_hash)
+VALUES ($1, $2)
+    RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: GetUserByUsername :one
+SELECT * FROM users WHERE username = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users ORDER BY id LIMIT $1 OFFSET $2;
