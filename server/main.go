9c73a63405bd661c685c233d084d8163adc37bb3
Song Yang <keesongyang4@gmail.com>
2025-07-21 00:26:55 +0800

    feat: update cors for dev

--- server/main.go
+++ server/main.go
@@ -31,7 +31,7 @@ func main() {
 
 	// Configure CORS
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:5173", "http://localhost:3000", "http://localhost:4173", "https://typedash.songyang.dev", "https://typedash-v2.netlify.app"}
+	config.AllowOrigins = []string{"http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:4173", "https://typedash.songyang.dev", "https://typedash-v2.netlify.app"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
 	config.AllowCredentials = true
