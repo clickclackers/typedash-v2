52cbaef753584ea4dde276056ccce0954852f937
s0ngyang <keesongyang4@gmail.com>
2025-12-06 21:09:15 +0800

    feat: update footer link

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 const Footer: FC = () => {
   return (
     <div className='mt-8'>
-      <Link href='https://github.com/raynertjx/typedash' isExternal>
+      <Link href='https://github.com/clickclackers/typedash-v2' isExternal>
         Made by Click Clackers.
       </Link>
     </div>
