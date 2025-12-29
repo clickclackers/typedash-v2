60d007642dc33d25642932f187c3daec15873c05
s0ngyang <keesongyang4@gmail.com>
2025-12-30 00:28:07 +0800

    chore: update about

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -3,10 +3,6 @@ import { FC } from 'react';
 const About: FC = () => {
   return (
     <div className='flex flex-col justify-center font-semibold gap-4'>
-      <p>
-        Made with love by Kee Song Yang and Rayner Toh Jing Xiang for Orbital
-        2023.
-      </p>
       <p>Heavily inspired by Monkeytype and Typeracer.</p>
       <p>We hope you enjoy using it as much as we enjoyed making it {'<3'}</p>
     </div>
